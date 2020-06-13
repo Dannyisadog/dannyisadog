c14c19f02275c3eb54e9d3a1935b992038cca8f1
Dannyisadog <dannyisadog10@gmail.com>
2020-06-13 22:44:48 +0800

    Add main page layout

---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>@yield('app-title')</title>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        @yield('navbar')
+        @yield('content')
+        @yield('footer')
+    </body>
+</html>
